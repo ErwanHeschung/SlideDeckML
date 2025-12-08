c06d5f7894eab5821ccc27474d277f42bb99c350
Heschung Erwan <erwan.heschung@etu.univ-cotedazur.fr>
2025-12-08 19:54:28 +0100

    feat: add vite for hot relaod and ts for better visibility

---
+++ revealJS/main.ts
@@ -0,0 +1,10 @@
+import Reveal from "reveal.js";
+import "reveal.js/dist/reveal.css";
+import "reveal.js/dist/theme/white.css";
+
+Reveal.initialize({
+    hash: true,
+    slideNumber: true,
+    width: "100%",
+    height: "100%"
+});
---
+++ revealJS/reveal.d.ts
@@ -0,0 +1,9 @@
+declare module "reveal.js" {
+    const Reveal: any;
+    export default Reveal;
+}
+
+declare module "reveal.js/plugin/*" {
+    const plugin: any;
+    export default plugin;
+}

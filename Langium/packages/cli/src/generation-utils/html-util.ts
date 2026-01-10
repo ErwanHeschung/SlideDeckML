4f53c4c33e75ec178e9b180d4675a9659285e524
Heschung Erwan <erwan.heschung@etu.univ-cotedazur.fr>
2026-01-10 15:39:17 +0100

    feat: add optional steps in codeblock

--- Langium/packages/cli/src/generation-utils/html-util.ts
+++ Langium/packages/cli/src/generation-utils/html-util.ts
@@ -1,4 +1,4 @@
-import { CodeBlock, Content, isHAlignOption, isLayoutTypeOption, isVAlignOption, LayoutBlock, LayoutStyle, MathBlock, MediaBlock, Presentation, Slide, TextBlock } from "slide-deck-ml-language";
+import { CodeBlock, Content, isCodeBlock, isFreeText, isHAlignOption, isImage, isLayoutBlock, isLayoutTypeOption, isMathBlock, isMediaBlock, isOrderedList, isRangeLineHighlight, isSimpleLineHighlight, isTextBlock, isUnorderedList, isVAlignOption, isVideo, LayoutStyle, MediaBlock, Presentation, Slide, TextBlock } from "slide-deck-ml-language";
 import { mediaSrc, renderVideo } from "./media-util.js";
 import { Prefixes } from "./prefix-registry-util.js";
 
@@ -32,65 +32,80 @@ function generateSlideHtml(slide: Slide, level: number): string {
 }
 
 function generateContentHtml(content: Content, level: number): string {
-    switch (content.$type) {
-        case 'Video':
-        case 'Image':
-            return generateMedia(content as MediaBlock, level);
-        case 'UnorderedList':
-        case 'OrderedList':
-        case 'FreeText':
-            return generateText(content as TextBlock, level);
-        case 'CodeBlock': {
-            const code = (content as CodeBlock).codeContent.join('\n');
-            const lines = code.split('\n').map((line:string) => pad(level + 1) + line).join('\n');
-            return `${pad(level)}<pre><code class="language-${(content as CodeBlock).language} ${Prefixes.getPrefix(content)}">\n${lines}\n${pad(level)}</code></pre>`;
-        }
-        case 'LayoutBlock': {
-            const children = (content as LayoutBlock).elements.map(e => generateContentHtml(e, level + 1)).join('\n');
-            
-            return `${pad(level)}<div class="layout ${getClassesFromLayout(content.layout as LayoutStyle)} ${Prefixes.getPrefix(content)}">\n${children}\n${pad(level)}</div>`;
-        }
-        case 'MathBlock': {
-            const formula = (content as MathBlock).content;
-            const lines = formula.split('\n').map(line => pad(level + 1) + line).join('\n');
-            return `${pad(level)}<div class="math ${Prefixes.getPrefix(content)}">\n${pad(level + 1)}$$\n${lines}\n${pad(level + 1)}$$\n${pad(level)}</div>`;
-        }
-        default:
-            return '';
+    if(isMediaBlock(content)) {
+        return generateMedia(content, level);
     }
+    if(isTextBlock(content)) {
+        return generateText(content, level);
+    }
+    if (isCodeBlock(content)) {
+        return generateCodeBlock(content, level);
+    }
+    if (isLayoutBlock(content)) {
+        const children = content.elements.map(e => generateContentHtml(e, level + 1)).join('\n');
+
+        return `${pad(level)}<div class="layout ${getClassesFromLayout(content.layout as LayoutStyle)} ${Prefixes.getPrefix(content)}">\n${children}\n${pad(level)}</div>`;
+    }
+    if(isMathBlock(content)) {
+        const formula = content.content;
+        const lines = formula.split('\n').map(line => pad(level + 1) + line).join('\n');
+        return `${pad(level)}<div class="math ${Prefixes.getPrefix(content)}">\n${pad(level + 1)}$$\n${lines}\n${pad(level + 1)}$$\n${pad(level)}</div>`;
+    }
+    return '';
+
 }
 
 function generateMedia(content: MediaBlock, level: number): string  {
-    switch (content.$type) {
-        case 'Video': {
-            const src = mediaSrc(content);
-            return `${pad(level)}${renderVideo(src,Prefixes.getPrefix(content))}`;
-        }
-        case 'Image': {
-            const src = mediaSrc(content);
-            return `${pad(level)}<img class="${Prefixes.getPrefix(content)}" src="${src}" alt="" />`;
-        }
+    if(isVideo(content)) {
+        const src = mediaSrc(content);
+        return `${pad(level)}${renderVideo(src,Prefixes.getPrefix(content))}`;
     }
+    if(isImage(content)) {
+        const src = mediaSrc(content);
+        return `${pad(level)}<img class="${Prefixes.getPrefix(content)}" src="${src}" alt="" />`;
+    }
+    return '';
 }
 
 function generateText(content: TextBlock, level: number): string  {
-    switch (content.$type) {
-        case 'FreeText': {
-            const text = content.inline ?? content.block ?? '';
-            const lines = text.split('\n').map(line => pad(level + 1) + line).join('\n');
-            return `${pad(level)}<p class="${Prefixes.getPrefix(content)}">\n${lines}\n${pad(level)}</p>`;
-        }
-        case 'UnorderedList': {
-            const items = content.items.map(i => pad(level + 1) + `<li>${i.text}</li>`).join('\n');
-            return `${pad(level)}<ul class="${Prefixes.getPrefix(content)}">\n${items}\n${pad(level)}</ul>`;
-        }
-        case 'OrderedList': {
-            const items = content.items.map(i => pad(level + 1) + `<li>${i.text}</li>`).join('\n');
-            return `${pad(level)}<ol class="${Prefixes.getPrefix(content)}">\n${items}\n${pad(level)}</ol>`;
-        }
+    if (isFreeText(content)) {
+        const text = content.inline ?? content.block ?? '';
+        const lines = text.split('\n').map(line => pad(level + 1) + line).join('\n');
+        return `${pad(level)}<p class="${Prefixes.getPrefix(content)}">\n${lines}\n${pad(level)}</p>`;
     }
+    if(isUnorderedList(content)) {
+        const items = content.items.map(i => pad(level + 1) + `<li>${i.text}</li>`).join('\n');
+        return `${pad(level)}<ul class="${Prefixes.getPrefix(content)}">\n${items}\n${pad(level)}</ul>`;
+    }
+    if(isOrderedList(content)) {
+        const items = content.items.map(i => pad(level + 1) + `<li>${i.text}</li>`).join('\n');
+        return `${pad(level)}<ol class="${Prefixes.getPrefix(content)}">\n${items}\n${pad(level)}</ol>`;
+    }
+    return '';
 }
 
+function generateCodeBlock(content: CodeBlock, level: number): string {
+    const code = content.codeContent;
+    const lines = code.split('\n').map((line: string) => pad(level + 1) + line).join('\n');
+    const codeHighlight = getCodeHighlight(content);
+    return `${pad(level)}<pre><code data-trim ${codeHighlight??""} class="language-${content.language} ${Prefixes.getPrefix(content)}">\n${lines}\n${pad(level)}</code></pre>`;
+}
+
+function getCodeHighlight(content: CodeBlock): string | undefined {
+    let linesHighlight = 'data-line-numbers="';
+    if (content.highlight) {
+        for (const line of content.highlight.lines) {
+            if (isSimpleLineHighlight(line)) {
+                linesHighlight += `${line.line}`;
+            } else if (isRangeLineHighlight(line)) {
+                linesHighlight += `${line.startLine}-${line.endLine}`;
+            }
+            linesHighlight += '|';
+        }
+        return linesHighlight + '"';
+    }
+    return undefined;
+}
 
 function pad(level: number) {
     return '\t'.repeat(level);
